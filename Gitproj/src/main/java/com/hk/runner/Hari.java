35dabd2a18f5b1d97c824d7d35789c057139ecf6
jacksrocks <jakahari016@gmail.com>
2025-04-04 17:32:35 +0530

    initial commit

---
+++ Gitproj/src/main/java/com/hk/runner/Hari.java
@@ -0,0 +1,15 @@
+package com.hk.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class Hari implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+    System.out.println("hello bosss");
+    System.out.println("how are you");
+
+	}
+
+}
